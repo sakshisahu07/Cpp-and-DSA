48aa13943d0435acd9c43ce4112799fbfddfefd1
sakshisahu07 <158145304+sakshisahu07@users.noreply.github.com>
2024-06-26 03:09:49 -0700

    Add files via upload
    This ia a merge sort

--- mergesort.cpp
+++ mergesort.cpp
@@ -44,3 +44,42 @@ int main(){
 	print(a,8);
 	
 }
+
+
+void merge(int a[],int low,int mid,int high){
+	int i=low,j=mid+1,size=high-low+1,k=0;
+	int temp[size];
+	while(i<=mid && j<=high){
+		if(a[i]<=a[j]){
+		temp[k]=a[i];
+		k++,i++;
+	}
+	else{
+		temp[k]=a[j];
+		k++,j++
+	}
+	while(i<=mid){
+		temp[k]=a[i];
+		k++,i++;
+	}
+	while(j<=high){
+		temp[k]=a[j];
+		k++,j++;
+	}
+}
+void merge_sort(int a[],int low, int high){
+	if(low<high){
+		int mid=(low+high)/2;
+		merge_sort(a,low,mid);
+		merge_sort(a,mid+1,high);
+		merge_sort(a,low,mid,high);
+	}
+}
+void print(int A[],int n){
+	for(int i=0;i<n;i++){
+		cout<<A[i]<<" ";
+	}
+}
+int main(){
+	int a[5]={3,4,2,1,5};
+}
